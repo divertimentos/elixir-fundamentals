de0e82d512a3db05ef1f3304751ed01f2cdbe206
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-02-11 17:07:53 -0300

    Unless

--- control_flow/if_else.exs
+++ control_flow/if_else.exs
@@ -1,11 +1,19 @@
 defmodule Statements do
   def if_else(num) do
     if num > 42 do
-      IO.puts("Higher than 100!")
+      IO.puts(" #{num} is higher than 100!")
     else
       IO.puts("Lower than 100!")
+    end
+  end
+
+  def unless_statement(num) do
+    unless num > 42 do
+      # Interpolation
+      IO.puts("#{num} < 42")
     end
   end
 end
 
 Statements.if_else(24)
+Statements.unless_statement(24)
